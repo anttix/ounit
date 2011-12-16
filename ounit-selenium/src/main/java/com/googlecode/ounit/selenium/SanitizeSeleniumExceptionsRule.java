dd79a5f1fcdf351294e9b55a68d530d8a1e71b69
anttix <anttix@localhost>
2011-12-16 18:26:39 +0200

    Revert "element not found" message change: too verbose

--- ounit-selenium/src/main/java/com/googlecode/ounit/selenium/SanitizeSeleniumExceptionsRule.java
+++ ounit-selenium/src/main/java/com/googlecode/ounit/selenium/SanitizeSeleniumExceptionsRule.java
@@ -24,7 +24,6 @@ package com.googlecode.ounit.selenium;
 import org.junit.rules.TestRule;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriverException;
 
 /**
@@ -55,12 +54,6 @@ public class SanitizeSeleniumExceptionsRule implements TestRule {
 					int idx = msg.indexOf("For documentation on this error");
 					if(idx > 0)
 						msg = msg.substring(0, idx);
-
-					if(e instanceof NoSuchElementException) {
-						msg += "Usually this happens if a required element " +
-								"is missing, page structure is wrong or HTML " +
-								"has errors in it.";
-					}
 					
 					throw new AssertionError(msg);
 				}
